f6baac3371fe18442b4b53f18e2cd536922047d7
imtiazziko <ziko.iut@gmail.com>
2020-02-14 17:11:09 -0500

    LaplacianShot clean update

--- run_mini.sh
+++ run_mini.sh
@@ -4,25 +4,25 @@ export CUDA_VISIBLE_DEVICES=0
 # train on mini imagenet
 #Simpleshot
 #model=resnet10.config
-# python ./src/train_lshot.py -c ./configs/mini/softmax/$model --data ./data/images/ --log-file /simpleshot.log --evaluate --enlarge
+# python ./src/train_lshot.py -c ./configs/mini/softmax/$model --data ./data/images/ --log-file /simpleshot.log --evaluate
 
 tune=True
 lmd=1.0 # Overridden when tune=True
 
 # ## Resnet 10
 #model=resnet10.config
-#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot --log-file /LaplacianShot.log --evaluate --enlarge
+#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot --log-file /LaplacianShot.log --evaluate
 # ##
 ## ## Resnet 18
 model=resnet18.config
-python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate
 #### WRN
 #model=wideres.config
-#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate
 ## ### Mobilenet
 #model=mobilenet.config
-#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate
 ## ### Densenet
 #model=densenet121.config
-#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+#python ./src/train_lshot.py -c ./configs/mini/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/images/ --lshot  --log-file /LaplacianShot.log --evaluate
 #
--- run_tiered.sh
+++ run_tiered.sh
@@ -4,25 +4,25 @@ export CUDA_VISIBLE_DEVICES=1
 # train on tiered imagenet
 #Simpleshot
 #model=resnet10.config
-# python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --data ./data/tiered-imagenet/data/ --log-file /simpleshot.log --evaluate --enlarge
+# python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --data ./data/tiered-imagenet/data/ --log-file /simpleshot.log --evaluate
 
 tune=True
 lmd=1.0
 
 # ## Resnet 10
 model=resnet10.config
-python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate
 # ##
 # ## Resnet 18
 model=resnet18.config
-python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate
 # ##
 ### WRN
 model=wideres.config
-python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate
 # ### Mobilenet
 model=mobilenet.config
-python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate
 # ### Densenet
 model=densenet121.config
-python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate --enlarge
+python ./src/train_lshot.py -c ./configs/tiered/softmax/$model --lmd $lmd --tune-lmd $tune  --data ./data/tiered-imagenet/data/ --lshot  --log-file /LaplacianShot.log --evaluate
